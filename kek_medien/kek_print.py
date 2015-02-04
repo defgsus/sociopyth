7ee115e5ef871ba4e5d7394a4e487dfd8fcab122
def.gsus- <stefan.berke@modular-audio-graphics.com>
2015-02-04 14:56:54 +0100

    add neato output - can't really comprehent, though

--- kek_medien/kek_print.py
+++ kek_medien/kek_print.py
@@ -8,11 +8,42 @@ from kek_tools import kek_read_company_xml, Company, Share
 
 
 
+def kek_export_neato(companies, filename):
+	"""Exports the owner relations to neato - not really useful."""
+	f = open(filename, "wt")
+	
+	c_id = {}
+	k = 0
+	# company nodes
+	f.write("graph {\n")
+	for c in companies.values():
+		# node id for graphviz
+		k = k + 1
+		cid = "n" + str(k)
+		c_id[c.name] = cid
+		f.write((cid + ' [label="' + c.name.replace('"', "'") + '"];\n').encode("UTF-8"))
+		
+	# edges
+	for c in companies.values():
+		cid = c_id[c.name]
+		for s in c.shares.values():
+			sid = c_id[s.name]
+			f.write((cid + " -- " + sid + ' [label="' + str(s.percent) + '%"];\n').encode("UTF-8"))
+			# neato crashes with this..
+			#f.write((cid + " -- " + sid + ' [len=' + str(100. / (1.+s.percent)) + "];\n").encode("UTF-8"))
+	
+	f.write("}")
+	f.close()
+
+
+
+
 ########################################################################
 
 
 companies = kek_read_company_xml("./kek_owner.xml")
 
+#kek_export_neato(companies, "kek_owner.dot"); quit()
 
 # print companies		
 for c in companies.values():
